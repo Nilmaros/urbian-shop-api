1220a88822a56d04a61885f3aa9b360a5818ca0f
mitchell.hernandez <mitchell.hernandez@menziesaviation.com>
2019-06-26 15:40:39 +0100

    test

--- queries.js
+++ queries.js
@@ -1,12 +1,12 @@
 const POOL = require('pg').Pool;
 
 const pool = new POOL({
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        database: process.env.DATABASE_URL,
-        port: process.env.DB_PORT
-    });
+    // user: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // host: process.env.DB_HOST,
+    database: process.env.DATABASE_URL
+    //port: process.env.DB_PORT
+});
 
 console.log(pool);
 console.log(process.env.DATABASE_URL);

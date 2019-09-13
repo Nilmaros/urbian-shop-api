7dc12d2834244b57268e411596947c62f8e2a44e
Hernandez Broadhurst <mitchell.hernandezbroadhurst@towerswatson.com>
2019-09-13 11:27:17 +0100

    testing

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var bodyparser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 8080;
 /* req stands for request, res stands for response */
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true, }));
-app.get('/product/all', db.GetAllProducts);
-app.get('/product/all/count', db.CountAllProducts);
-app.get('/product/id/:id', db.GetProductById);
-app.get('/product/offset/:offset', db.GetProductByOffset);
-app.delete('/product/:id', db.DeleteProduct);
-app.post('/product/new', db.PostProduct);
-app.post('/product/update', db.UpdateProduct); // Need to review
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({ extended: true, }));
+// app.get('/product/all', db.GetAllProducts);
+// app.get('/product/all/count', db.CountAllProducts);
+// app.get('/product/id/:id', db.GetProductById);
+// app.get('/product/offset/:offset', db.GetProductByOffset);
+// app.delete('/product/:id', db.DeleteProduct);
+// app.post('/product/new', db.PostProduct);
+// app.post('/product/update', db.UpdateProduct); // Need to review
 console.log("I am alive!!");
 app.listen(PORT, function () { console.log('Express is listening port:' + PORT + '!'); })
